1baa879014e233fdfc0815e5c126317217dda25d
Scott Hoisington <hoyzington@gmail.com>
2020-01-24 04:25:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,7 +98,7 @@ class Student
       WHERE grade = 10
       LIMIT 1
     SQL
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
