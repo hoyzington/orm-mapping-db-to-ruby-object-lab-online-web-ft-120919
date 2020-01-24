5707d3f9e2585d0290953355b7b249687f8d0f34
Scott Hoisington <hoyzington@gmail.com>
2020-01-24 03:49:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,10 +22,10 @@ class Student
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * 
-      FROM songs
+      FROM students
       WHERE name = ?
     SQL
-    row = DB[:conn].execute(sql)
+    row = DB[:conn].execute(sql, name)
     self.new_from_db(row)
   end
   
